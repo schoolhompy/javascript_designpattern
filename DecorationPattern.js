a9f3ddb57726572308644ca03529def7c91de32b
sugoigroup <schoolhompy@gmail.com>
2018-06-27 19:30:05 +0900

    Update DecorationPattern.js

--- DecorationPattern.js
+++ DecorationPattern.js
@@ -48,16 +48,19 @@ decorationIce.prototype.getPrice = function () {
 var orderCoffee0 = new blackCoffee();
 var payment0 = new americano(orderCoffee0);
 console.log(payment0.getPrice());
+//4000
 
 //Order BlackCoffe with Cream
 var orderCoffee1 = new blackCoffee();
 var payment1 = new decorationCream(orderCoffee1);
 var payment1_1 = new decorationIce(payment1);
 console.log(payment1_1.getPrice());
+//5200 = 4000 + 1000 + 200
 
 //Order Cafuchino
 var orderCoffee2 = new cafuchino();
 var payment2 = new decorationCream(orderCoffee2);
 console.log(payment2.getPrice());
+//6500 = 5500 + 1000 
 
 
