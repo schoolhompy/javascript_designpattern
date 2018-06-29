8169f7c7fedf0cbdc2134aef5df84be16168412a
sugoigroup <schoolhompy@gmail.com>
2018-06-29 17:41:02 +0900

    Create sample.js
    Struct javascript

---
+++ sample.js
@@ -0,0 +1,81 @@
+/**
+ * Javascript Comment Template.
+ * Description
+ *
+ * @author KTH
+ */
+(function ($, P, undefined)
+{
+	'use strict';
+	
+
+	
+	var Template = function (options)
+	{
+		/* ************************************************************************
+		 *  Public attributes
+		 * ************************************************************************/
+		
+			
+		/* ************************************************************************
+		 *  Private attributes
+		 * ************************************************************************/
+
+		// static variables
+			
+		// instance variables
+		
+		/* ************************************************************************
+		 *  Private methods
+		 * ************************************************************************/
+		
+		/**
+		 * Constructor Method
+		 * 
+		 * @private
+		 * @memberOf Template
+		 */
+		function _construct()
+		{;
+		};
+
+		/**
+		 * sample private method
+		 * 
+		 * @private
+		 * @param bulabula
+		 * @memberOf Template
+		 */
+		function _myPrivate()
+		{
+			return ;
+		}
+				
+			
+		/* ************************************************************************
+		 *  Public methods
+		 * ************************************************************************/
+		
+		/**
+		 *  sample public method
+		 * 
+		 * @param 
+		 * @return 
+		 * 
+		 * @public
+		 * @memberOf Template
+		 */
+		this.publicMethod = function (arg)
+		{
+			return rval;
+		};
+
+		/*
+		 * Invoke the constructor
+		 */
+		_construct.call(this);
+	};
+	
+	
+	
+})(jQuery, P);
